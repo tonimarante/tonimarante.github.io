1d54bffc69e78860bd01bfcfe7cfcc6510490c94
tonimarante <tonimarante@gmail.com>
2022-11-06 21:24:22 +0100

    diumenge, 6 de novembre de 2022, 21:24:28 CET

--- indexhtmlcreator.sh
+++ indexhtmlcreator.sh
@@ -7,8 +7,7 @@ echo "<UL>" >> "$OUTPUT"
 for filepath in $(find "$ROOT"  -maxdepth 10 -mindepth 1 -name '*.html' | sort); do
     FITXER=$(basename "$filepath")
     RUTA=${filepath:22}
-    #echo "    <LI> <font size="+1" face="Arial"> <a href=\"$RUTA\">  $FITXER</a></LI>" >> "$OUTPUT"
-    echo "    <LI> <font size="+1" face="Liberation Mono"> <a href=\"$RUTA\">  $FITXER</a></LI>" >> "$OUTPUT"
+    echo "    <LI> <font size="+1" face="Arial"> <a href=\"$RUTA\">  $FITXER</a></LI>" >> "$OUTPUT"
 done
 echo "</UL>" >> "$OUTPUT"
 echo "</body>" >> "$OUTPUT"
