5fccd7dc2c1c78c1e4776c283b38c3aa702daadf
tonimarante <tonimarante@gmail.com>
2022-11-06 20:05:12 +0100

    20221106-2005

--- indexhtmlcreator.sh
+++ indexhtmlcreator.sh
@@ -6,10 +6,16 @@ cat "$HOME/Vimwiki_html/READMEbase.htm" > "$OUTPUT"
 echo "<UL>" >> "$OUTPUT"
 for filepath in $(find "$ROOT"  -maxdepth 10 -mindepth 1 -name '*.html' | sort); do
     FITXER=$(basename "$filepath")
-    echo "    <LI> <font size="+1" face="Arial"> <a href=\"$filepath\">  $FITXER</a></LI>" >> "$OUTPUT"
+    RUTA=${filepath:22}
+    echo "    <LI> <font size="+1" face="Arial"> <a href=\"$RUTA\">  $FITXER</a></LI>" >> "$OUTPUT"
 done
 echo "</UL>" >> "$OUTPUT"
 echo "</body>" >> "$OUTPUT"
 echo "</html>" >> "$OUTPUT"
+
+git fetch
+git add .
+git commit
+git push
 
 
