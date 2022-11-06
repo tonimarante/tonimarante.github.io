c325f670703426af52e26b5bba2e8bd5f95a760d
tonimarante <tonimarante@gmail.com>
2022-11-06 23:09:23 +0100

    diumenge, 6 de novembre de 2022, 23:09:26 CET

--- indexhtmlcreator.sh
+++ indexhtmlcreator.sh
@@ -10,8 +10,10 @@ for filepath in $(find "$ROOT"  -maxdepth 10 -mindepth 1 -name '*.html' | sort);
     echo "    <LI> <font size="+1" face="Arial"> <a href=\"$RUTA\">  $FITXER</a></LI>" >> "$OUTPUT"
 done
 echo "</UL>" >> "$OUTPUT"
-echo "</body>" >> "$OUTPUT"
-echo "</html>" >> "$OUTPUT"
+cat "/home/bmw/Vimwiki_html/README_end.htm" > "$OUTPUT"
+
+#echo "</body>" >> "$OUTPUT"
+#echo "</html>" >> "$OUTPUT"
 
 #git pull
 git add .
